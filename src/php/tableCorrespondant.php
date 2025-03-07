043c27664a9a01f643f0bae040a2662602bd9d0b
Rhazoul <developpements.drn@gmail.com>
2025-03-07 09:32:00 +0100

    06/03

--- src/php/tableCorrespondant.php
+++ src/php/tableCorrespondant.php
@@ -20,13 +20,37 @@ function tableCorrespondantShow()
          $e = oci_error();
          trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
      }
-    $req = "select * from labos where ville = 'LOOS'";
+    $req = "select * from CORRESPONDANTS"; //labos where ville = 'LOOS'
 
     $stid = oci_parse($conn, $req);
     oci_execute($stid);
     $nrows = oci_fetch_all($stid, $results);
 
-    echo "Connexion réussie";
+    if ($nrows > 0 ) {
+
+        
+
+    echo '<div class="row align-items-start">
+      <table class="table table-striped mb-0">
+        <thead>
+          <tr class="sticky">
+            <th scope="col">Code Correspondant</th>
+            <th scope="col">Code Labo</th>
+            <th scope="col">Nom</th>
+            <th scope="col">Prénom</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr style="height: 52px;">
+            <td class="u-table-cell">65</td>
+            <td class="u-table-cell">84</td>
+            <td class="u-table-cell">HESSNER</td>
+            <td class="u-table-cell">Thomas</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>';
+    }
 
     oci_free_statement($stid);
     oci_close($conn);

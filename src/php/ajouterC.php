7eab5653c5c687799a32c4224802197e35477bfb
Rhazoul <developpements.drn@gmail.com>
2025-05-28 14:59:08 +0200

    correctif 3

--- src/php/ajouterC.php
+++ src/php/ajouterC.php
@@ -66,6 +66,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
         $checkrow = oci_fetch_assoc($checkST);
         if ($checkrow['CNT'] > 0){
             $erreurs[] = "Le code correspondant << {$codeCorresp} >> existe déjà.";
+            die;
         }
                     //execution et commit
         $ok = oci_execute($stid, OCI_COMMIT_ON_SUCCESS);

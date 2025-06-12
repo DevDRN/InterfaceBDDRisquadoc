5d309fb4083f775ef223de663853620dee4226e7
Rhazoul <developpements.drn@gmail.com>
2025-06-12 15:55:43 +0200

    user mise en page

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+//avoir la date du jour en inscription auto
 const today = new Date();
 const jour = String(today.getDate()).padStart(2,'0');
 const mois = String(today.getMonth() + 1).padStart(2,'0');
@@ -6,3 +7,10 @@ const dateFormat = `${jour}/${mois}/${annee}`;
 
 
 document.getElementById('dateMaj').value = dateFormat;
+
+//automatisation de l'username
+function setUsername() {
+    const prenom = document.getElementById('prenom').value.trim().toLowerCase();
+    const nom = document.getElementById('nom').value.trim().toLowerCase();
+    document.getElementById('username').value = prenom && nom ? '${prenom}.${nom}' : '';
+}
--- src/php/sendMail.php
+++ src/php/sendMail.php
@@ -7,6 +7,7 @@ use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader (created by composer, not included with PHPMailer)
 require 'vendor/autoload.php';
+include 'user.php';
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
@@ -15,29 +16,29 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp-mail.outlook.com';                     //Set the SMTP server to send through
+    $mail->Host       = 'smtp.office365.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'svc_power365@chu-lille.fr';                     //SMTP username
     $mail->Password   = 'Z5BDUVwg8uwZXB';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
     $mail->setFrom('svc_power365@chu-lille.fr', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-    $mail->addAddress('ellen@example.com');               //Name is optional
+    $mail->addAddress($sendTo, "$nom. .$prenom");     //Add a recipient
+/*     $mail->addAddress('ellen@example.com');               //Name is optional
     $mail->addReplyTo('info@example.com', 'Information');
     $mail->addCC('cc@example.com');
     $mail->addBCC('bcc@example.com');
-
+ */
     //Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+/*     $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
     $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-
+ */
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->Subject = $subject;
+    $mail->Body    = $body;
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();

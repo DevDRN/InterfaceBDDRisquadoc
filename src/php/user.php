408f712d0bbc986ad1239a407d3c45ead288b500
Rhazoul <developpements.drn@gmail.com>
2025-06-20 15:16:40 +0200

    user
    buton retour à la liste

--- src/php/user.php
+++ src/php/user.php
@@ -312,6 +312,10 @@ oci_execute($stmt);
     </tbody>
 </table>
 
+  <footer class="sticky-bottom">
+      <a href="dashboard.php" class="btn btn-secondary">Retour à liste</a>
+  </footer>
+
   <script src="../js/main.js"></script>
   <script src="../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
   <script src="../../node_modules/jquery/dist/jquery.min.js"></script>

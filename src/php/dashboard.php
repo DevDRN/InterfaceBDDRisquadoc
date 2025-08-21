e03db489e2caff8c1b2eb2542ff230864d4af8d9
Rhazoul <developpements.drn@gmail.com>
2025-08-21 15:51:54 +0200

    logo finit

--- src/php/dashboard.php
+++ src/php/dashboard.php
@@ -66,10 +66,11 @@ $nrows = oci_fetch_all($stid, $results);
 <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
   <div class="col-md-3 mb-2 mb-md-0">
     <a href="dashboard.php" class="d-inline-flex link-body-emphasis text-decoration-none" title="Retour au menu">
-      <svg class="bi" width="40" height="32" role="img" aria-label="GIL">
-        <use xlink:href="../img/GIL.png"></use>
+      <img src="../img/GILtransparent.svg" alt="GIL" class="logo">
+<!--       <svg class="logo" aria-label="GIL">
+        <use xlink:href="../img/GILtransparent.svg"></use>
       </svg>
-    </a>
+ -->    </a>
   </div>
   <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
     <li>
